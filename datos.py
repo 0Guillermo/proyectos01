6f71d4d33c0ea98d9a4c7a53853acd6a2e054a45
0Guillermo <guillermo341630@gmail.com>
2021-08-06 20:07:35 -0300

    mi base

---
+++ datos.py
@@ -0,0 +1,8 @@
+def datos():
+    print("esto sera la base de datos")
+    mis_datos = open("datos.txt","a")
+    nombre = input("ingrese su nombre ")
+    mis_datos.write(nombre)
+    mis_datos = open("datos.txt","r")
+    print(mis_datos.readlines())
+datos()
